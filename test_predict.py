84c08223e52cf462677aa1de4f32a20f27db13cb
DiegoVentura23 <gentileventura@gmail.com>
2024-02-16 15:23:52 -0500

    firstcomiit

---
+++ test_predict.py
@@ -0,0 +1,14 @@
+import requests
+
+endpoint = 'http://localhost:8080/predict'
+
+input_data= {
+    'user_id': '100',
+    'n_visits':10,
+
+
+}
+
+response = requests.post(url=endpoint, json= input_data)
+print(response)
+print(response.json())
